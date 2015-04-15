d7f2395dce7e17b5d4976455ec0ed9a597281d00
YEONWOO86 <wkdqkrrns@hanmail.net>
2015-04-15 19:52:07 +0900

    third

---
+++ spring_thirdweek/src/main/java/spring_thirdweek/test/di/MainClass.java
@@ -0,0 +1,22 @@
+package spring_thirdweek.test.di;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX_di.xml");
+		
+		AdminConnection connection = ctx.getBean("adminConnection", AdminConnection.class);
+		System.out.println("adminID : " + connection.getAdminId());
+		System.out.println("adminPW : " + connection.getAdminPw());
+		System.out.println("sub_adminID : " + connection.getSub_adminId());
+		System.out.println("sub_adminPW : " + connection.getSub_adminPw());
+		
+		ctx.close();
+		
+	}
+	
+}
